aceaf2253770a1364a1e92c9c90067eda0bc35f8
ssidikov <sardorbek.sidikov7@gmail.com>
2024-07-17 18:16:52 +0200

    Add snapshot test for SignIn component

--- js/pages/signIn/index.snapshot.test.js
+++ js/pages/signIn/index.snapshot.test.js
@@ -1,3 +1,12 @@
-import SignIn from './index'
+import SignIn from "./index";
 
 // Écrivez votre test ici
+describe("SignIn Snapshot Test Suites", () => {
+  it("should match snapshot", () => {
+    expect(SignIn).toMatchInlineSnapshot(`
+Object {
+  "render": [Function],
+}
+`);
+  });
+});
