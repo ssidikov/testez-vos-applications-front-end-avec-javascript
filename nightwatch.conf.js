74aee40843177caabcfd5bb026b8a9cf98f109f1
ssidikov <sardorbek.sidikov7@gmail.com>
2024-07-17 17:42:34 +0200

    Update chromedriver

---
+++ nightwatch.conf.js
@@ -0,0 +1,7 @@
+// a mettre dans nightwatch.conf.js
+const chromedriver = require("chromedriver");
+module.exports = (function (settings) {
+  settings.test_workers = false;
+  settings.webdriver.server_path = chromedriver.path;
+  return settings;
+})(require("./nightwatch.json"));
--- nightwatch/first_e2e_test.js
+++ nightwatch/first_e2e_test.js
@@ -1,20 +1,20 @@
 module.exports = {
-    test: client => {
-        client
-            .url('http://127.0.0.1:5500/')
-            .waitForElementVisible('.main-title', 10 * 1000)
-            .assert.visible('input[type=email]')
-            .setValue('input[type=email]', 'thomas@facadia.com')
-            .assert.visible('input[type=password]')
-            .setValue('input[type=password]', 'azerty')
-            .assert.visible('button[type=submit]')
-            .click('button[type=submit]')
-            .waitForElementVisible('.section-title', 10 * 1000)
-            .assert.containsText('.section-title', 'Vos capteurs')
-            .assert.visible('.sensor-info-btn')
-            .click('.sensor-info-btn')
-            .waitForElementVisible('.section-title', 10 * 1000)
-            .assert.containsText('.section-title', 'Détails du capteur #7')
-            .end()
-    }
-}
+  test: (client) => {
+    client
+      .url("http://127.0.0.1:5500/")
+      .waitForElementVisible(".main-title", 10 * 1000)
+      .assert.visible("input[type=email]")
+      .setValue("input[type=email]", "thomas@facadia.com")
+      .assert.visible("input[type=password]")
+      .setValue("input[type=password]", "azerty")
+      .assert.visible("button[type=submit]")
+      .click("button[type=submit]")
+      .waitForElementVisible(".section-title", 10 * 1000)
+      .assert.containsText(".section-title", "Vos capteurs")
+      .assert.visible(".sensor-info-btn")
+      .click(".sensor-info-btn")
+      .waitForElementVisible(".section-title", 10 * 1000)
+      .assert.containsText(".section-title", "Détails du capteur #7")
+      .end();
+  },
+};
